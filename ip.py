9777976072effb152db4741c911748011adcf8b7
Trapani99 <83341549+Trapani99@users.noreply.github.com>
2021-04-28 11:38:17 +0200

    Update ip.py

--- ip.py
+++ ip.py
@@ -14,7 +14,7 @@ def get_Host_name_IP():
             ip = ni.ifaddresses(x)[ni.AF_INET][0]['addr']
             list_ip += "\n" + x + ": " + ip
         ip = get('https://api.ipify.org').text
-        indirizzi_IP = "Hostname: "+str(host_name)+"\nIndirizzi IP del dispositivo: "+list_ip+"\nIndirizzo IP pubblico: {}".format(ip)+"\nData e ora attuale: "+str(dt_string)
+        indirizzi_IP = "Hostname: "+str(host_name)+"\nDevice's IP: "+list_ip+"\nPublic IP: {}".format(ip)+"\nActual date and time: "+str(dt_string)
         print (indirizzi_IP)
 
     except Exception as err:
